326fc316b5e050c61b9b247908ea142fad75bc16
Tara Nguyen <70169207+tara-nguyen@users.noreply.github.com>
2020-08-29 18:02:54 -0500

    Add goalkeeper
    A description of the changes is in README.md file.

--- MyFirstGameUsingClasses.py
+++ MyFirstGameUsingClasses.py
@@ -1,33 +1,49 @@
 import pygame, sys, random
-import ClassesForMyFirstGame as game
+import NonplayerClasses as nonplayer
+import PlayerClasses as player
 
 pygame.init()   # start pygame
 
 pygame.display.set_caption('My First Game')   # game title
-screenSize = 550, 700   # screen size
+screenSize = 400, 500   # screen size
 
 # create objects that will appear on the screen
-bg = game.Background(screenSize)
-goal = game.Goal(screenSize)
-player = game.Player(screenSize)
-ball = game.Ball(screenSize)
+bg = nonplayer.Background(screenSize)
+goal = nonplayer.Goal(screenSize)
+goalkeeper = player.Goalkeeper(screenSize)
+striker = player.Outfielder(screenSize)
+ball = nonplayer.Ball(screenSize)
+
+# load objects into pygame
+bg.load('grass')
+goal.load('goalLeft', 'goalMiddle', 'goalRight')
+goalkeeper.load('playerFoot', 'playerBody')
+striker.load('playerFoot', 'playerBody')
+ball.load('ball')
 
 # draw the objects onto the screen
-bg.blit('grass')
-goal.blit('goalLeft', 'goalMiddle', 'goalRight')
-player.blit('playerFoot', 'playerBody', 'feet')
-ball.blit('ball')
-player.blit('playerFoot', 'playerBody', 'body')
+bg.blit()
+goal.blit()
+goalkeeper.blitFeet(rotate=-180)
+striker.blitFeet()
+ball.blit()
+goalkeeper.blitBody(rotate=-180)
+striker.blitBody()
 
 # x-coordinates and size of the goal posts
 goalPosts = goal.getPosts()
 
 # list of everything on the screen
-allThings = [bg.grass, goal.left, goal.middle, goal.right, player.lFoot,
-             player.rFoot, ball.ball, player.body]
+allThings = [bg.grass, goal.left, goal.middle, goal.right, goalkeeper.lFoot,
+             goalkeeper.rFoot, striker.lFoot, striker.rFoot, ball.ball,
+             goalkeeper.body, striker.body]
 # list of the positions of everything on the screen
-allPos = [bg.pos] + goal.getPos() + player.getPos()
-allPos.insert(-1, ball.getPos())
+allPos = [bg.pos] + goal.getPos() + goalkeeper.getStartPos()[:2] + \
+         striker.getStartPos()[:2] + [ball.getStartPos()] + \
+         [goalkeeper.getStartPos()[2]] + [striker.getStartPos()[2]]
+# indexes of the striker's left foot and of his body
+strikerLFootIndex = allThings.index(striker.lFoot)
+strikerBodyIndex = allThings.index(striker.body)
     
 bg.frame   # initialize clock
 
@@ -40,28 +56,30 @@ while playing:
             pygame.quit()
             sys.exit()   # prevents getting an error message when quitting the game
 
-    player.moved = False
+    striker.moved = False
     pressedKeys = pygame.key.get_pressed()
     # process keys used for player movements
     pressed, direction, moveType = bg.processMoveKeys(pressedKeys)
     if pressed in bg.moveKeys:
         # one of the keys used for player movements has been pressed
-        player.move(moveType, direction, ball.center, ball.getCenterPos())
+        striker.move(moveType, direction, ball.center, ball.getCenterPos())
     if pressedKeys[pygame.K_s] == 1:
-        # 's' key has been pressed --> player will kick the ball
+        # 's' key has been pressed --> striker will kick the ball
         # choose the foot that will kick the ball
-        player.kickBall(ball.getCenterPos(), ball.center, allThings, allPos)
-        if player.touchedBall:
+        striker.kickBall(ball.getCenterPos(), ball.center,
+                         allThings, allPos, strikerLFootIndex)
+        if striker.touchedBall:
             # foot has touched the ball --> ball will move
             # nearest distance to the player that the ball can get
-            minDist = ball.center[1] + player.bodyCenterStart[1] + \
-                      player.feetOut - 5
+            minDist = ball.center[1] + striker.bodyCenterStart[1] + \
+                      striker.feetOut - 3
             # move ball
             ball.moveBall(
-                player.getBodyAngle(ball.getCenterPos()), random.uniform(28,32),
-                goalPosts, player.getRotation(), player.getMidpoint(), minDist,
+                striker.getBodyAngle(ball.getCenterPos()), random.uniform(18,22),
+                goalPosts, striker.getRotation(), striker.getMidpoint(), minDist,
                 allThings, allPos, pressedKeys)
             
-    if player.moved:
-        player.updatePlayer(allThings, allPos)
+    if striker.moved:
+        striker.updatePlayer(allThings, allPos,
+                             strikerLFootIndex, strikerBodyIndex)
     bg.updateDisplay()
